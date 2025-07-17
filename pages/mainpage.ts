d24f0847c722ff9ea70dfec33d736a7cdb9508eb
Robert Pecz <rob.pecz@gmail.com>
2025-07-17 10:56:47 +0200

    Testcase: Login with invalid email and valid password (#26)

--- pages/mainpage.ts
+++ pages/mainpage.ts
@@ -17,8 +17,8 @@ class MainPage {
         signInButton : () => this.page.locator('//input[@value="Log in"]'),
         loggedInUserLink : (loggedInName: string) => this.page.getByRole('link', {name: loggedInName}),
         loginUnsuccesfulErrorLabel : () => this.page.locator("//div[@class='validation-summary-errors']/span"),
-        credentialErrorLabel : () => this.page.locator("//div[@class='validation-summary-errors']/ul/li")
-
+        credentialErrorLabel : () => this.page.locator("//div[@class='validation-summary-errors']/ul/li"),
+        enterValidEmailErrorLabel : () => this.page.locator("//span[@class='field-validation-error']/span[@for='Email']")
     }
 
     async userLogIn(email: string, password: string) {
@@ -26,6 +26,13 @@ class MainPage {
         await this.elements.emailTextBox().fill(email);
         await this.elements.passwordTextBox().fill(password);
         await this.elements.signInButton().click();
+    }
+
+    //The reason why need to press enter because email validation happening on frontend side which could trigger with a keyboard press.
+    async userInvalidEmailInput(email: string) {
+        await this.elements.loginButton().click();
+        await this.elements.emailTextBox().fill(email);
+        await this.elements.emailTextBox().press('Enter');
     }
 }
 
