0ff965755e7cf7b87ebe2b578678526b949eb1fa
Robert Pecz <rob.pecz@gmail.com>
2026-02-07 12:59:19 +0100

    Add test.describe

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,63 +3,71 @@ import mainpageData from '../fixtures/mainpageData.json';
 import { test, expect } from '@playwright/test';
 import MainPage from '../pages/mainpage';
 
-test.beforeEach(async ({ page }) => {
-  console.log(`Running ${test.info().title}`);
-  await page.goto('/');
-});
-
-test('Has title', async ({ page }) => {
-  await test.step('Validate that page title is what expected', async () => {
-    await expect(page).toHaveTitle('Demo Web Shop');
-  });
-});
-
-test('User log in successful', async ({ page }) => {
-  const mainPage = new MainPage(page);
-
-  await test.step('User log in', async () => {
-    await mainPage.userLogIn(mainpageData.email, mainpageData.password);
+test.describe('Login tests', () => {
+  test.beforeEach(async ({ page }) => {
+    console.log(`Running ${test.info().title}`);
+    await page.goto('/');
   });
 
-  await test.step('Validate user is logged in', async () => {
-    await expect(mainPage.elements.loggedInUserLink(mainpageData.email)).toBeVisible();
-  });
-});
-
-test('User log in invalid password', async ({ page }) => {
-  const mainPage = new MainPage(page);
-
-  await test.step('User log in', async () => {
-    await mainPage.userLogIn(mainpageData.email, mainpageData.invalidPassword);
+  test('Has title', async ({ page }) => {
+    await test.step('Validate that page title is what expected', async () => {
+      await expect(page).toHaveTitle('Demo Web Shop');
+    });
   });
 
-  await test.step('Validate that correct error messages appear', async () => {
-    await expect(mainPage.elements.loginUnsuccesfulErrorLabel()).toHaveText(errorMessage.loginUnsuccesfulErrorMessage);
-    await expect(mainPage.elements.credentialErrorLabel()).toHaveText(errorMessage.credentialsIncorrectErrorMessage);
-  });
-});
+  test('User log in successful', async ({ page }) => {
+    const mainPage = new MainPage(page);
 
-test('User log in invalid email', async ({ page }) => {
-  const mainPage = new MainPage(page);
+    await test.step('User log in', async () => {
+      await mainPage.userLogIn(mainpageData.email, mainpageData.password);
+    });
 
-  await test.step('User log in with invalid email', async () => {
-    await mainPage.userInvalidEmailInput(mainpageData.invalidEmail);
-  });
-
-  await test.step('Validate that correct error messages appear', async () => {
-    await expect(mainPage.elements.enterValidEmailErrorLabel()).toHaveText(errorMessage.invalidEmailLoginErrorMessage);
-  });
-});
-
-test('User log in empty password', async ({ page }) => {
-  const mainPage = new MainPage(page);
-
-  await test.step('User log in', async () => {
-    await mainPage.userLogIn(mainpageData.email, '');
+    await test.step('Validate user is logged in', async () => {
+      await expect(mainPage.elements.loggedInUserLink(mainpageData.email)).toBeVisible();
+    });
   });
 
-  await test.step('Validate that correct error messages appear', async () => {
-    await expect(mainPage.elements.loginUnsuccesfulErrorLabel()).toHaveText(errorMessage.loginUnsuccesfulErrorMessage);
-    await expect(mainPage.elements.credentialErrorLabel()).toHaveText(errorMessage.credentialsIncorrectErrorMessage);
+  test('User log in invalid password', async ({ page }) => {
+    const mainPage = new MainPage(page);
+
+    await test.step('User log in', async () => {
+      await mainPage.userLogIn(mainpageData.email, mainpageData.invalidPassword);
+    });
+
+    await test.step('Validate that correct error messages appear', async () => {
+      await expect(mainPage.elements.loginUnsuccesfulErrorLabel()).toHaveText(
+        errorMessage.loginUnsuccesfulErrorMessage,
+      );
+      await expect(mainPage.elements.credentialErrorLabel()).toHaveText(errorMessage.credentialsIncorrectErrorMessage);
+    });
+  });
+
+  test('User log in invalid email', async ({ page }) => {
+    const mainPage = new MainPage(page);
+
+    await test.step('User log in with invalid email', async () => {
+      await mainPage.userInvalidEmailInput(mainpageData.invalidEmail);
+    });
+
+    await test.step('Validate that correct error messages appear', async () => {
+      await expect(mainPage.elements.enterValidEmailErrorLabel()).toHaveText(
+        errorMessage.invalidEmailLoginErrorMessage,
+      );
+    });
+  });
+
+  test('User log in empty password', async ({ page }) => {
+    const mainPage = new MainPage(page);
+
+    await test.step('User log in', async () => {
+      await mainPage.userLogIn(mainpageData.email, '');
+    });
+
+    await test.step('Validate that correct error messages appear', async () => {
+      await expect(mainPage.elements.loginUnsuccesfulErrorLabel()).toHaveText(
+        errorMessage.loginUnsuccesfulErrorMessage,
+      );
+      await expect(mainPage.elements.credentialErrorLabel()).toHaveText(errorMessage.credentialsIncorrectErrorMessage);
+    });
   });
 });
